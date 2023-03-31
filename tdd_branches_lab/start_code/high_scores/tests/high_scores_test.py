22175feb7a010ea6d90b11d1102986dfde83045a
Gregor <gregor.home@btinternet.com>
2023-03-31 10:38:49 +0100

    finishing high scores latest function

--- tdd_branches_lab/start_code/high_scores/tests/high_scores_test.py
+++ tdd_branches_lab/start_code/high_scores/tests/high_scores_test.py
@@ -9,42 +9,51 @@ class HighScoresTest(unittest.TestCase):
     
     # Tests
     def setUp(self):
-        self.scores = [2, 4, 8, 10, 15]
+        self.scores = [2, 4, 5, 10, 8, 3]
 
     # Test latest score (the last thing in the list)
     def test_can_get_latest_score(self):
-        expected_value = 15
+        expected_value = 3
         actual_value = latest(self.scores)
         self.assertEqual(expected_value, actual_value)
 
 
     # Test personal best (the highest score in the list)
     def test_can_get_personal_list(self):
-        expected_value = 15
+        expected_value = 10
         actual_value = personal_best(self.scores)
         self.assertEqual(expected_value, actual_value)
 
     # Test top three from list of scores
     def test_can_get_top_three(self):
-        expected_value = [15,10,8]
+        expected_value = [10,8,5]
         actual_value = personal_top_three(self.scores)
         self.assertEqual(expected_value, actual_value)
 
     # Test ordered from highest tp lowest
     def test_can_get_hightest_to_lowest(self):
-        expected_value = [15,10,8,4,2]
+        expected_value = [10,8,5,4,3,2]
         actual_value = highest_to_lowest(self.scores)
         self.assertEqual(expected_value, actual_value)
 
     # Test top three when there is a tie
     def test_top_three_when_there_is_tie(self):
-        scores_tie = [2, 15, 8, 10, 15, 15]
-        expected_value = "Tie between 15 and 15"
-        actual_value = top_three(scores_tie)
+        scores = [2, 8, 4, 5, 10, 8, 3]
+        expected_value = [10, 8, 8]
+        actual_value = personal_top_three(scores)
         self.assertEqual(expected_value, actual_value)
 
 
     # Test top three when there are less than three
+    def test_can_get_top_three_when_less_than_three(self):
+        scores = [1,2]
+        expected_value = [2,1]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected_value, actual_value)
 
     # Test top three when there is only one
-    
+    def test_can_get_top_three_when_less_than_three(self):
+        scores = [1]
+        expected_value = [1]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected_value, actual_value)
